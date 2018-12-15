7b8e7876c3744d58f5499622ea4ce76cbf6e0b79
dyingcarl <carluner@gmail.com>
2018-12-15 01:00:52 -0500

    changed the intro text, added fade effects and change-button lock state for lucky color page.

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+function fade(){
+  $(".displayArea").animate({opacity: 0.1},2000);
+  setTimeout(randomColor,2000);
+  $(".interaction button").attr("disabled",true);
+}
 
+function enableBtn(){
+  $(".interaction button").attr("disabled",false);
+}
 
 function randomColor(){
+  var a = $(".displayArea")
   var redCode = Math.floor(Math.random()*256);
   var greenCode = Math.floor(Math.random()*256);
   var blueCode = Math.floor(Math.random()*256);
   var colorCode = "rgb("+redCode+","+greenCode+","+blueCode+")";
-  $(".displayArea").css("color",colorCode);
-  $(".displayArea").css("borderColor",colorCode);
-  $(".displayArea").html("<span class='ColorCode'>RED: "+redCode+ " GREEN: "+greenCode+" BLUE: "+blueCode+"</span>");
+  a.css("borderColor",colorCode);
+  a.animate({opacity: 1.0},2000);
+  a.html("<span class='ColorCode'>RED: "+redCode+ " GREEN: "+greenCode+" BLUE: "+blueCode+"</span>");
+  setTimeout(enableBtn,2000);
 }
 
-
-// document.querySelector(".interaction button").setAttribute("onclick","randomColor();");
-$(".interaction button").on("click", randomColor);
-$(".displayArea").on("click",randomColor);
+$(".interaction button").on("click", fade);
+// $(".displayArea").on("click",randomColor);
